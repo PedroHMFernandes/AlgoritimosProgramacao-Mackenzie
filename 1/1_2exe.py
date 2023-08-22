084faaaeaeb26c856f81d162f68cf2f7f632711d
Pedro Fernandes <pedrovosvisk10@gmail.com>
2023-08-22 10:40:00 -0300

    Create 1_2exe.py

---
+++ 1/1_2exe.py
@@ -0,0 +1,4 @@
+# 2. Faça um programa que converta metros para milímetros (1m = 1000mm).
+metros = float(input('Digite metros para passar para mm: '))
+milimetros = metros * 1000
+print(f'{metros}m = {milimetros}mm')
