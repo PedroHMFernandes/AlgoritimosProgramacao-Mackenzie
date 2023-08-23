d11ab4e408a720c238685cae0356645ad86c9801
Pedro Fernandes <pedrovosvisk10@gmail.com>
2023-08-23 17:35:56 -0300

    Update 1_4exe.py

--- 22_08_23/1_4exe.py
+++ 22_08_23/1_4exe.py
@@ -1,4 +1,4 @@
-# 4. Faça um programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês 
+# 4. Faça um programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês
 # (modifiquei para dia).
 # Calcule e mostre o total do seu salário no referido mês.
 
