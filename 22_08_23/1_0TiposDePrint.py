0e77b1a2de1f577bacb7541915750c6ad549f49a
Pedro Fernandes <pedrovosvisk10@gmail.com>
2023-08-23 17:35:43 -0300

    Update 1_0TiposDePrint.py

--- 22_08_23/1_0TiposDePrint.py
+++ 22_08_23/1_0TiposDePrint.py
@@ -3,13 +3,13 @@ b = 34.88
 c = a + b
 print('Valor de a =', a)
 print('Valor de b = ', b, 'e tal coisa', a)
-print('Valor de a= %d' %a)
-print('Valor de b = %.2f' %b)
+print('Valor de a= %d' % a)
+print('Valor de b = %.2f' % b)
 print(f'f-strings são as melhores; a + b = {a+b}')
 print(f'a + b = {a+b:.5f}')
 
 salario_hora = int(input())
-hora_mes= int(input())
+hora_mes = int(input())
 salario_mes = salario_hora * hora_mes
 print(f'{salario_mes}')
 
