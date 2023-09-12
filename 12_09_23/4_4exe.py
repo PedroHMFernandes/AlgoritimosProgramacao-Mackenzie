5cd85bed9d18e62e486796da2f02a5cae3ca28ba
Pedro Fernandes <pedrovosvisk10@gmail.com>
2023-09-12 10:37:50 -0300

    Create 4_4exe.py

---
+++ 12_09_23/4_4exe.py
@@ -0,0 +1,9 @@
+# 4) Faça um programa que tendo como dados de entrada o código de região de localização do cliente,
+# o nome do cliente, o número de peças vendidas e o nome do vendedor; calcule e informe o valor do
+# frete, a comissão do vendedor e o lucro obtido com a venda, sabendo-se que:
+#  - O valor do frete depende da quantidade transportada e da região;
+#  - Custo por peça = R$ 7,00;
+#  - Custo total = custo por peça * número de peças vendidas;
+#  - Valor total da venda = custo total acrescido de 50%;
+#  - Comissão do vendedor = 6,5 % do valor total da venda;
+#  - Lucro = Valor total venda – custo total – comissão do vendedor;
